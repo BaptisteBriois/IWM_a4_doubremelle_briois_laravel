50ccf9dd4905e979ceb6599eb6a84c1a79817b9d
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2017-10-13 10:46:55 +0200

    ajax post task

--- public/js/show.js
+++ public/js/show.js
@@ -86,12 +86,12 @@ $("#categoryForm").submit(function (e) {
         url: Routes["category_store"],
         data: {
             project_id: $(this).data('project-id'),
-            title: $("#categoryTitle").val()
+            title: $(this).find("input").val()
         },
         dataType: "json"
     }).done(function (response, status, jqXHR) {
-        $("#categoryTitle").val("");
-        $("#newCategory").before('<div class="col-category">\n' + '<div class="panel panel-default">\n' + '<div class="panel-heading">\n' + response.category.title + '\n' + '</div>\n' + '<div class="panel-drag panel-body"></div>\n' + '<div class="panel-footer">\n' + '<form class="taskForm" data-project-id="' + response.category.project_id + '" data-category-id="' + response.category.id + '">\n' + '<div class="form-group">\n' + '<div class="form-group">\n' + '<input type="text" class="form-control" id="taskTitle" placeholder="Titre" name="title">\n' + '</div>\n' + '</div>\n' + '</form>\n' + '</div>\n' + '</div>\n' + '</div>');
+        $("#categoryForm").find("input").val("");
+        $("#newCategory").before('<div class="col-category">' + '<div class="panel panel-default">' + '<div class="panel-heading">' + response.category.title + '' + '</div>' + '<div class="panel-drag panel-body"></div>' + '<div class="panel-footer">' + '<form class="taskForm" data-project-id="' + response.category.project_id + '" data-category-id="' + response.category.id + '">' + '<div class="form-group">' + '<div class="form-group">' + '<input type="text" class="form-control" id="taskTitle" placeholder="Titre" name="title">' + '</div>' + '</div>' + '</form>' + '</div>' + '</div>' + '</div>');
     });
 });
 
@@ -103,11 +103,14 @@ $(".taskForm").submit(function (e) {
         url: Routes["task_store"],
         data: {
             project_id: $(this).data('project-id'),
-            category_id: $(this).data('category_id'),
+            category_id: $(this).data('category-id'),
             title: $(this).find("input").val()
         },
         dataType: "json"
-    }).done(function (response, status, jqXHR) {});
+    }).done(function (response, status, jqXHR) {
+        $(".taskForm[data-category-id=" + response.task.category_id + "]").find("input").val("");
+        $(".col-category[data-category-id=" + response.task.category_id + "]").find(".categoryTasks").append('<div class="panel panel-default">' + '<div class="panel-body">' + response.task.title + '</div>' + '</div>');
+    });
 });
 
 /***/ })
