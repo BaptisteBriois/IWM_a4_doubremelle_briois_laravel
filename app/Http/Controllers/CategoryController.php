7c5bae3b6acac2100654cb7ead7a541eb21c2047
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2017-10-15 23:31:53 +0200

    Fix import Auth in Controllers

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
